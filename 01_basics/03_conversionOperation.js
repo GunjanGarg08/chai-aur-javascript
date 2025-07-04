4c0a0286bfa67d4b1b6a8d67ac6d951c3e518a32
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-04 18:16:10 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = 33
-let score2 = "33"
-let score3 = "33abc"
-let score4 = null
+// let score = 33
+// let score2 = "33"
+// let score3 = "33abc"
+// let score4 = null
 
-console.log(typeof score); // number
-console.log(typeof(score)); // number
-console.log(typeof score2); // string
+// console.log(typeof score); // number
+// console.log(typeof(score)); // number
+// console.log(typeof score2); // string
 
 // let valueInNumber = Number(score2)
 // console.log(typeof valueInNumber); // number
@@ -15,23 +15,63 @@ console.log(typeof score2); // string
 // console.log(valueInNumber); // this will show us that "NaN" is stored inside the variable
 // above is same result for "undefined" also
 
-let valueInNumber = Number(score4)
-console.log(typeof valueInNumber); // number
-console.log(valueInNumber); // this will show us that "0" is stored inside the variable
+// let valueInNumber = Number(score4)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // this will show us that "0" is stored inside the variable
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); // true
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
 
 // 1 => true; 0 => false
 // "" => false
 // "gunjan" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber); // 33
-console.log(typeof stringNumber); // string
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); // 33
+// console.log(typeof stringNumber); // string
+
+// *********************** Operations *********************** //
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // power
+// console.log(2/2);
+// console.log(2%2); // modulus or remainder
+
+// let str1 = "hello"
+// let str2 = " gunjan"
+// let str3 = str1 + str2;
+// console.log(str3); // hello gunjan
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true+); // Error
+// console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2;
+
+// let gameCounter = 100;
+// gameCounter++; // Postfix
+// console.log(gameCounter); // 101
+
+// Link to Study
+// https://tc39.es//ecma262/multipage/abstract-operations.html#sec-type-conversion
