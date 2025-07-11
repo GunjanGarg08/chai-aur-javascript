819b461fedbe39da37cc57f36ed1d685aa33b8ec
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-11 18:17:05 +0000

    function1 done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,55 @@
+function sayMyName() {
+    console.log("G");
+    console.log("U");
+    console.log("N");
+    console.log("J");
+    console.log("A");
+    console.log("N");
+}
+
+// sayMyName, yeh sirf function ka reference hai... isko kuch bhi output main nahi aayega
+// sayMyName(), function ko aise execute krte hai
+
+// function addTwoNumbers(number1, number2) { // Parameters
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers() // NaN, because haamne function ka arguments number1 and number2 ko koi value hi ni di
+// addTwoNumbers(3, 4); // 7, Arguments
+// // So, below cases main thodi problem aati hai because function ko nahi pta hota ki kaise add krne hai alag numbers nahi hai toh... issiliye uska aise if else krke condition lagakr dekh sakte hai ki datatype agar number hai toh add krdo vrna nahi... toh voh hum logics baad main dekhlenge
+// addTwoNumbers(3, "4"); // 34
+// addTwoNumbers(3, "a"); // 3a
+// addTwoNumbers(3, null) // 3
+// Jab function create krte hai toh uss time PARAMETERS kehte hai and function call krte time joh values pass krte hai unhe ARGUMENTS bolte hai
+
+// toh yeh aise variable main store nahi hota
+// const result = addTwoNumbers(3, 5); // 8
+// console.log("Result: ", result); // Result: undefined
+
+// correct way
+// function addTwoNumbers(number1, number2) {
+//     // let result = number1 + number2;
+//     // return result;
+//     // console.log("Gunjan"); // yeh kabhi print nahi hoga... because isse pehle hum function sa return krr chuke hai
+//     return number1 + number2;
+// }
+// const result = addTwoNumbers(3, 5);
+// console.log("Result: ", result); // Result: 8
+
+function loginUserMessage(username) { // username = "sam", yeh default value bhi de saktein hai aise... toh aise uski value kabhi empty ya undefined nahi aaygi... agar humne function call ka time koi value pass ni ki toh yeh "sam just logged in" dikha dega... aur agar koi value pass ki toh yeh default value override hojaygi and haame pass ki hui value output main dikh jaygi
+    // if(username === undefined) {
+    //     console.log("Please enter a username");
+    //     return; // if this condition became true then output will be "Please enter a username \n undefined"
+    // }
+    // OR
+    if(!undefined) {
+        console.log("Please enter a username");
+        return;
+    }
+    return `${username} just logged in`
+}
+loginUserMessage("Gunjan"); // kuch bhi print nahi hoga.. because values pass krdi... function na return bhi krdi... but kahi parr unhe print toh ni karaya na issliye
+console.log(loginUserMessage("Gunjan")); // Gunjan just logged in
+console.log(loginUserMessage("")); // just logged in, Empty String passed toh kuch ni aayega
+console.log(loginUserMessage()); // undefined just logged in ; but jab koi value hi pass ni ki... tab undefined aata hai, toh iske liye bhi if statement sa check karr saktein hai
+
