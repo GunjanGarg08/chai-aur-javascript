f2d574d012b8e592b566fc701a71e126983893d7
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-18 20:23:11 +0000

    conditionals started

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if
+
+// "condition" should be "true" then only if statement will work, otherwise if it will be "false" then it will not work
+// if (condition) {
+
+// }
+
+// const isUserLoggedIn = true;
+// if (isUserLoggedIn) {
+
+// }
+
+// Comparison Operators
