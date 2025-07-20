ca2e9dc0afb461cdd1a0686ad2aa0ec670243a76
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-20 20:11:08 +0000

    conditionals continued

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -30,12 +30,22 @@ if ( 2 === "2" ) {
 // so, here both the console statements will be printed
 // so, here we will use else statement
 
-const temperature = 41;
+// const temperature = 41;
 
-if( temperature === 41 ) {
-    console.log("less than 50");
-} else {
-    console.log("temperature is greater than 50");
-}
-console.log("Execute");
+// if( temperature === 41 ) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
 // less than 50, Execute
+
+// Important
+// const score = 200;
+// if (score > 100) {
+//     const power = "fly";
+//     console.log(`User Power: ${power}`); // User Power: fly
+// }
+
+// console.log(`User Power: ${power}`); // inside "if" toh koi problem nahi aaygi... but outside main ERROR aayega... because "power" variable ki scope sirf "if" statements ka andar hi valid hai... bhar nahi
+
