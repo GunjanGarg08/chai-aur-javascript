82729678572e1a0e243baa91eef49524b20a4391
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-25 16:59:14 +0000

    loops started

---
+++ 05_iterations/one.js
@@ -0,0 +1,15 @@
+// for loop
+
+// Basic Syntax
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if (element == 5) {
+        console.log("5 is best number");
+    }
+    console.log(element);
+}
