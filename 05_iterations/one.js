0df47ddc24f18f9f3106b8733a12cc384ae513ae
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-26 17:28:14 +0000

    loops continued

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -13,3 +13,11 @@ for (let index = 0; index <= 10; index++) {
     }
     console.log(element);
 }
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer Loop Value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner Loop Value ${j} and Inner Loop ${i}`);
+        console.log(i + '*' + j + ' = ' + i*j);
+    } 
+}
