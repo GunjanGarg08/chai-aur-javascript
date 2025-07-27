e00b1a40969efc1182461ed960bd64977989f050
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-27 17:07:23 +0000

    loops continued

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -21,3 +21,11 @@ for (let i = 0; i <= 10; i++) {
         console.log(i + '*' + j + ' = ' + i*j);
     } 
 }
+
+let myArray = ["flash", "batman", "superman"];
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
