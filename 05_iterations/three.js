c4ea730536e8c7edda7e7e8169aa996d379a741c
Gunjan Garg <155942878+GunjanGarg08@users.noreply.github.com>
2025-07-31 16:58:00 +0000

    high order array loops continues

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -31,3 +31,24 @@ map.set('Fr', "France");
 // map.set('IN', "India"); // This won't be stored again in the map, as it's the repeated value
 
 console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+// }
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+// const  myObject = {
+//     'game1': 'NFS',
+//     'game2': 'Spiderman'
+// }
+const  myObject = { // doesn't work on this syntax also
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value); // error - myObject is not iterable
+// }
